d0b5ef5f12d2fade1e5bac9c6d1a43e6d39b43d9
gonzaliaga <gonzaliaga@gmail.com>
2023-10-02 20:20:17 -0300

    cambios en nosotros

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,11 @@
 export default function About() {
     return (
-      <div className="h-full grid grid-cols-2 mt-96 align-middle">
+      <div className="h-full grid grid-cols-2 mt-46 align-middle">
         <div>
           <p> Acerca de Nosotros</p>
         </div>
         <div>
-          <span>Somos un emprendimiento familiar de campo, dedicadas a la recría de gallinas ponedoras de razas bronw Nick, Nick chick y inta negras. -
+          <span>Somos un emprendimiento familiar de campo, dedicadas a la recría de gallinas ponedoras de razas brown Nick, Nick chick e inta negras. -
 Producimos animales de calidad ya que se presta especial atención en su higiene, salud y alimentación desde su nacimiento hasta las 16 semanas de vida, es decir en la pre postura. -
 Consideramos que su crianza a piso (libre de jaulas y sin estrés) con un alimento preparado en nuestro establecimiento y de acuerdo a la necesidad de cada etapa de vida del ave, asegura que la gallina, en etapa de postura, tenga una producción permanente y de calidad. -
 Tenemos una importante experiencia en la calidad de la producción ya que, posterior a la entrega de las aves a nuestros clientes, llevamos a cabo un seguimiento personalizado de efectividad, comprobando un alto porcentaje de producción de huevos. -
